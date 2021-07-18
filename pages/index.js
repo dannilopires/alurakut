1e9adc9730c73c6463b024f52bbe0fba5717f1b3
dannilo <dannilo-pires@hotmail.com>
2021-07-18 16:45:12 -0300

    add profilerelationsboxwrapper

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 function ProfileSidebar(propriedades) {
 	return (
@@ -15,7 +16,7 @@ export default function Home() {
 
   const githubUser = "dannilopires";
 
-  const pessoas = ["juunegreiros", "omaurosouto", "peas", "badtuxx"]
+  const pessoas = ["juunegreiros", "omariosouto", "peas", "badtuxx", "anuraghazra", "peixebabel"];
 
   return (
     <>
@@ -27,14 +28,32 @@ export default function Home() {
 
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box> 
-            Bem-Vindo
+            <h1 className="title">
+              Bem-Vindo(a)
+            </h1>
+            <OrkutNostalgicIconSet/>
           </Box>
         </div>
       
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-          <Box> 
-            Comunidades
-          </Box>
+          <ProfileRelationsBoxWrapper> 
+            <h2 className="smallTitle">
+              Pessoas({pessoas.length})
+            </h2>
+
+            <ul>
+              {pessoas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                  )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
